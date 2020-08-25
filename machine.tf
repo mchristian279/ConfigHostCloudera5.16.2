2832be32671df3c1c277a2537f5f91c7339a6573
Michael Christian <m.christian279@gmail.com>
2020-08-25 00:12:59 -0300

    Create machine.tf
    add machine

---
+++ machine.tf
@@ -0,0 +1,61 @@
+# Script Proviona Ambiente Cluster Cloudera com 3 VMs
+# Autor: Michael Christian
+# github: mchristian279
+
+#Provider
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+#Imagem
+resource "libvirt_volume" "os_image" {
+  name   = "os_image"
+  pool   = "Dados"
+  source = var.disk_img
+}
+
+#Volume
+resource "libvirt_volume" "volume" {
+  name           = "cloudera-${count.index}"
+  pool           = "Dados"
+  base_volume_id = libvirt_volume.os_image.id
+  count          = var.instance_count
+}
+
+#Rede
+resource "libvirt_network" "vm_network" {
+
+  name      = var.vm_network_name
+  addresses = ["${var.vm_network_addresses}"]
+  domain    = var.domain_name
+  mode      = "nat"
+  dhcp {
+    enabled = false
+  }
+
+  dns {
+    local_only = true
+  }
+
+  autostart = true
+}
+
+#VM
+resource "libvirt_domain" "domain" {
+  name   = "cloudera-${count.index}"
+  memory = "8024"
+  vcpu   = "2"
+
+  network_interface {
+    network_id     = libvirt_network.vm_network.id
+    hostname       = "cloudera-${count.index}"
+    addresses      = ["${var.vm_addresses[count.index]}"]
+    wait_for_lease = true
+
+  }
+
+  disk {
+    volume_id = libvirt_volume.volume[count.index].id
+  }
+  count = var.instance_count
+}
