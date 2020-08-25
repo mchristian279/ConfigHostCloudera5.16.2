259e3016d273fab5d390cc8d380f09930ec3f536
Michael Christian <m.christian279@gmail.com>
2020-08-25 00:13:39 -0300

    Create output.tf
    add output

---
+++ output.tf
@@ -0,0 +1,15 @@
+# Script Proviona Ambiente Cluster Cloudera com 3 VMs
+# Autor: Michael Christian
+# github: mchristian279
+
+output "Disco" {
+  value = libvirt_volume.volume.*.id
+}
+
+output "Rede" {
+  value = libvirt_network.vm_network.id
+}
+
+output "IP" {
+  value = libvirt_domain.domain.*.network_interface.0.addresses
+}
